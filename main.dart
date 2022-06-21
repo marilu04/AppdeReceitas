2833f107ff6c8c5c9784ee5d4dfb4828a848f980
marilu04 <103127649+marilu04@users.noreply.github.com>
2022-06-21 11:32:10 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:untitled5/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      const MaterialApp(
+        home: homePage(),
+      ),
+  );
+}
